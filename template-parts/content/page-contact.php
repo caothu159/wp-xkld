fe66de9847883db431eaa5ddae2615687ad74fd5
Trần Ngọc Đức <caothu91@gmail.com>
2019-06-25 22:38:35 +0700

    contact page

--- template-parts/content/page-contact.php
+++ template-parts/content/page-contact.php
@@ -17,15 +17,15 @@
                 <div class="col-md-7 form-right">
                     <ul>
                         <li>
-                            <i class="fas fa-map-marker-alt"></i>
+                            <i class="fa fa-map-marker-alt"></i>
                             <p>Trung tâm tư vấn xuất khẩu lao động Nhật - Việt</p>
                         </li>
                         <li>
-                            <i class="fas fa-phone"></i>
+                            <i class="fa fa-phone"></i>
                             <p>0986111979 - 02439728234</p>
                         </li>
                         <li>
-                            <i class="fab fa-viber"></i>
+                            <i class="fa fa-viber"></i>
                             <p>0986111979 (Mr Cường)</p>
                         </li>
                         <li>
@@ -33,7 +33,7 @@
                             <p>0986111979 (Mr Cường)</p>
                         </li>
 
-                        <li class="c-footmap-info__hint">
+                        <li class="info">
                             <p><i>Nếu bạn không tiện gọi điện hay nhắn tin trao đổi
                                     ngay lúc này, đừng ngần ngại để lại thông tin, chúng tôi sẽ gọi lại và tư
                                     vấn cho bạn.</i></p>
