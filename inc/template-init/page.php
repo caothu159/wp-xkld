a65ccc50ac6d4072e3137cf88edde60a88d8b482
Trần Ngọc Đức <caothu91@gmail.com>
2019-07-08 15:13:12 +0700

    split career page

--- inc/template-init/page.php
+++ inc/template-init/page.php
@@ -4,6 +4,7 @@ get_template_part( 'inc/template-init/page/home' );
 get_template_part( 'inc/template-init/page/contact' );
 get_template_part( 'inc/template-init/page/about' );
 get_template_part( 'inc/template-init/page/career' );
+get_template_part( 'inc/template-init/page/career', 'image' );
 
 function create_pages_fly( $page_name, $page_content = 'Starter content' ) {
     // Insert the post into the database
